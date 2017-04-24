c78e8e0c67b5ab6f023ffa46bdd1233fe1b0dbfb
Yixuan Qiu <yixuan.qiu@cos.name>
2017-04-24 01:14:32 -0400

    softmax function

--- Activation/Sigmoid.h
+++ Activation/Sigmoid.h
@@ -18,8 +18,8 @@ public:
 	}
 
 	// Apply the Jacobian matrix J to a vector f
-	// g = J * f
-	// J = d_a / d_z = diag(a * (1 - a))
+	// J = d_a / d_z = diag(a .* (1 - a))
+	// g = J * f = a .* (1 - a) .* f
 	// Z = [z1, ..., zn], G = [g1, ..., gn], F = [f1, ..., fn]
 	// Note: When entering this function, Z and G may point to the same matrix
 	static inline void apply_jacobian(const Matrix& Z, const Matrix& A, const Matrix& F, Matrix& G)
