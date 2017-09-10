0f2e46ab6a329d81d054e0919c4a44282d6487ad
Yixuan Qiu <yixuan.qiu@cos.name>
2017-09-10 14:45:20 -0400

    new way to obtain loss function value

--- include/Callback/VerboseCallback.h
+++ include/Callback/VerboseCallback.h
@@ -17,13 +17,13 @@ class VerboseCallback: public Callback
 public:
     void post_training_batch(const Network* net, const Matrix& x, const Matrix& y)
     {
-        Scalar loss = net->loss(y);
+        const Scalar loss = net->get_output()->loss();
         std::cout << "[Epoch " << m_epoch_id << ", batch " << m_batch_id << "] Loss = " << loss << std::endl;
     }
 
     void post_training_batch(const Network* net, const Matrix& x, const IntegerVector& y)
     {
-        Scalar loss = net->loss(y);
+        Scalar loss = net->get_output()->loss();
         std::cout << "[Epoch " << m_epoch_id << ", batch " << m_batch_id << "] Loss = " << loss << std::endl;
     }
 };
