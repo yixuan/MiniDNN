8dfc28d456cda3e0e33b0a1ff14b521cb8c52961
Yixuan Qiu <yixuanq@gmail.com>
2020-03-27 23:26:34 -0400

    tab to space

--- include/Utils/Enum.h
+++ include/Utils/Enum.h
@@ -15,20 +15,20 @@ namespace internal
 // Enumerations for hidden layers
 enum LAYER_ENUM
 {
-	FULLY_CONNECTED = 0,
+    FULLY_CONNECTED = 0,
     CONVOLUTIONAL,
-	MAX_POOLING
+    MAX_POOLING
 };
 
 // Convert a hidden layer type string to an integer
 inline int layer_id(const std::string& type)
 {
     if (type == "FullyConnected")
-    	return FULLY_CONNECTED;
+        return FULLY_CONNECTED;
     if (type == "Convolutional")
-    	return CONVOLUTIONAL;
+        return CONVOLUTIONAL;
     if (type == "MaxPooling")
-    	return MAX_POOLING;
+        return MAX_POOLING;
 
     throw std::invalid_argument("[function layer_id]: Layer is not of a known type");
     return -1;
@@ -37,29 +37,29 @@ inline int layer_id(const std::string& type)
 // Enumerations for activation functions
 enum ACTIVATION_ENUM
 {
-	IDENTITY = 0,
+    IDENTITY = 0,
     RELU,
-	SIGMOID,
-	SOFTMAX,
-	TANH,
-	MISH
+    SIGMOID,
+    SOFTMAX,
+    TANH,
+    MISH
 };
 
 // Convert an activation type string to an integer
 inline int activation_id(const std::string& type)
 {
     if (type == "Identity")
-    	return IDENTITY;
+        return IDENTITY;
     if (type == "ReLU")
-    	return RELU;
+        return RELU;
     if (type == "Sigmoid")
-    	return SIGMOID;
+        return SIGMOID;
     if (type == "Softmax")
-    	return SOFTMAX;
+        return SOFTMAX;
     if (type == "Tanh")
-    	return TANH;
+        return TANH;
     if (type == "Mish")
-    	return MISH;
+        return MISH;
 
     throw std::invalid_argument("[function activation_id]: Activation is not of a known type");
     return -1;
@@ -68,8 +68,8 @@ inline int activation_id(const std::string& type)
 // Enumerations for output layers
 enum OUTPUT_ENUM
 {
-	REGRESSION_MSE = 0,
-	BINARY_CLASS_ENTROPY,
+    REGRESSION_MSE = 0,
+    BINARY_CLASS_ENTROPY,
     MULTI_CLASS_ENTROPY
 };
 
@@ -77,11 +77,11 @@ enum OUTPUT_ENUM
 inline int output_id(const std::string& type)
 {
     if (type == "RegressionMSE")
-    	return REGRESSION_MSE;
+        return REGRESSION_MSE;
     if (type == "MultiClassEntropy")
-    	return BINARY_CLASS_ENTROPY;
+        return BINARY_CLASS_ENTROPY;
     if (type == "BinaryClassEntropy")
-    	return MULTI_CLASS_ENTROPY;
+        return MULTI_CLASS_ENTROPY;
 
     throw std::invalid_argument("[function output_id]: Output is not of a known type");
     return -1;
