d1539b52ecb0491fb542f62b6a0c865c48db2f00
Yixuan Qiu <yixuanq@gmail.com>
2021-04-10 20:15:20 +0800

    formatting

--- include/Callback.h
+++ include/Callback.h
@@ -1,5 +1,5 @@
-#ifndef CALLBACK_H_
-#define CALLBACK_H_
+#ifndef MINIDNN_CALLBACK_H_
+#define MINIDNN_CALLBACK_H_
 
 #include <Eigen/Core>
 #include "Config.h"
@@ -28,38 +28,34 @@ class Network;
 ///
 class Callback
 {
-    protected:
-        typedef Eigen::Matrix<Scalar, Eigen::Dynamic, Eigen::Dynamic> Matrix;
-        typedef Eigen::RowVectorXi IntegerVector;
+protected:
+    using Matrix = Eigen::Matrix<Scalar, Eigen::Dynamic, Eigen::Dynamic>;
+    using IntegerVector = Eigen::RowVectorXi;
 
-    public:
-        // Public members that will be set by the network during the training process
-        int m_nbatch;   // Number of total batches
-        int m_batch_id; // The index for the current mini-batch (0, 1, ..., m_nbatch-1)
-        int m_nepoch;   // Total number of epochs (one run on the whole data set) in the training process
-        int m_epoch_id; // The index for the current epoch (0, 1, ..., m_nepoch-1)
+public:
+    // Public members that will be set by the network during the training process
+    int m_nbatch;   // Number of total batches
+    int m_batch_id; // The index for the current mini-batch (0, 1, ..., m_nbatch-1)
+    int m_nepoch;   // Total number of epochs (one run on the whole data set) in the training process
+    int m_epoch_id; // The index for the current epoch (0, 1, ..., m_nepoch-1)
 
-        Callback() :
-            m_nbatch(0), m_batch_id(0), m_nepoch(0), m_epoch_id(0)
-        {}
+    Callback() :
+        m_nbatch(0), m_batch_id(0), m_nepoch(0), m_epoch_id(0)
+    {}
 
-        virtual ~Callback() {}
+    virtual ~Callback() {}
 
-        // Before training a mini-batch
-        virtual void pre_training_batch(const Network* net, const Matrix& x,
-                                        const Matrix& y) {}
-        virtual void pre_training_batch(const Network* net, const Matrix& x,
-                                        const IntegerVector& y) {}
+    // Before training a mini-batch
+    virtual void pre_training_batch(const Network* net, const Matrix& x, const Matrix& y) {}
+    virtual void pre_training_batch(const Network* net, const Matrix& x, const IntegerVector& y) {}
 
-        // After a mini-batch is trained
-        virtual void post_training_batch(const Network* net, const Matrix& x,
-                                         const Matrix& y) {}
-        virtual void post_training_batch(const Network* net, const Matrix& x,
-                                         const IntegerVector& y) {}
+    // After a mini-batch is trained
+    virtual void post_training_batch(const Network* net, const Matrix& x, const Matrix& y) {}
+    virtual void post_training_batch(const Network* net, const Matrix& x, const IntegerVector& y) {}
 };
 
 
 } // namespace MiniDNN
 
 
-#endif /* CALLBACK_H_ */
+#endif // MINIDNN_CALLBACK_H_
--- include/Callback/VerboseCallback.h
+++ include/Callback/VerboseCallback.h
@@ -1,5 +1,5 @@
-#ifndef CALLBACK_VERBOSECALLBACK_H_
-#define CALLBACK_VERBOSECALLBACK_H_
+#ifndef MINIDNN_CALLBACK_VERBOSECALLBACK_H_
+#define MINIDNN_CALLBACK_VERBOSECALLBACK_H_
 
 #include <Eigen/Core>
 #include <iostream>
@@ -14,29 +14,26 @@ namespace MiniDNN
 ///
 /// \ingroup Callbacks
 ///
-/// Callback function that prints the loss function value in each mini-batch training
+/// Callback function that prints the loss function value in each mini-batch training.
 ///
 class VerboseCallback: public Callback
 {
-    public:
-        void post_training_batch(const Network* net, const Matrix& x, const Matrix& y)
-        {
-            const Scalar loss = net->get_output()->loss();
-            std::cout << "[Epoch " << m_epoch_id << ", batch " << m_batch_id << "] Loss = "
-                      << loss << std::endl;
-        }
+public:
+    void post_training_batch(const Network* net, const Matrix& x, const Matrix& y) override
+    {
+        const Scalar loss = net->get_output()->loss();
+        std::cout << "[Epoch " << m_epoch_id << ", batch " << m_batch_id << "] Loss = " << loss << std::endl;
+    }
 
-        void post_training_batch(const Network* net, const Matrix& x,
-                                 const IntegerVector& y)
-        {
-            Scalar loss = net->get_output()->loss();
-            std::cout << "[Epoch " << m_epoch_id << ", batch " << m_batch_id << "] Loss = "
-                      << loss << std::endl;
-        }
+    void post_training_batch(const Network* net, const Matrix& x, const IntegerVector& y) override
+    {
+        Scalar loss = net->get_output()->loss();
+        std::cout << "[Epoch " << m_epoch_id << ", batch " << m_batch_id << "] Loss = " << loss << std::endl;
+    }
 };
 
 
 } // namespace MiniDNN
 
 
-#endif /* CALLBACK_VERBOSECALLBACK_H_ */
+#endif // MINIDNN_CALLBACK_VERBOSECALLBACK_H_
